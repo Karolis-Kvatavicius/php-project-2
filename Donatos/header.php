a6263aa788bcdd760e9fb3531fe3a5b90eac926d
Donata-K <39554183+Donata-K@users.noreply.github.com>
2018-08-30 13:04:54 +0300

    bla

--- Donatos/header.php
+++ Donatos/header.php
@@ -1,24 +1,15 @@
 <?php
-// if(!isset($_SESSION['sesija']) || !$_SESSION['sesija'] == true){
-//     header('Location:http://localhost/php-project-2/Donatos/login.php');
-//     exit;
-// }
- if(isset($_POST['logout'])) {
+if(isset($_GET['logout']) && $_GET['logout'] == true) {
    session_destroy();
-   header('Location:http://localhost/php-project-2/Donatos/login.php');
-   exit;
- }
-
-
+}
+$_SESSION['username'] = '';
 $servername = "localhost";
 $username = 'root';
 $password = '123';
-
 $conn = mysqli_connect($servername, $username, $password, 'wordpress2');
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
 }
-
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,78 +23,6 @@ if (!$conn) {
 <body>
 <nav>
 <ul id="bar">
-<?php switch(@$_SESSION['username']) {
-case 'Jonas': ?>
-  <li class="menu menu-hover">
-    <a href="#" class="button">Jono</a>
-    <ul class="submenu">
-      <li><a href="#">Menu item 1</a></li>
-      <li><a href="#">Menu item 2</a></li>
-      <li><a href="#">Menu item 3</a></li>
-      <li><a href="#">Menu item 4</a></li>
-    </ul>
-  </li>
-  <li class="menu menu-right menu-click">
-    <a class="button" href="http://localhost/php-project-2/Donatos/login.php">Logout</a>
-  </li>
-<?php break;
-case 'Marius':?>
-  <li class="menu menu-hover">
-    <a href="#" class="button">Mariaus</a>
-    <ul class="submenu">
-      <li><a href="http://localhost/PHP-project-2/Mariaus/saras.html">Šaras</a></li>
-      <li><a href="#">Menu item 2</a></li>
-      <li><a href="#">Menu item 3</a></li>
-      <li><a href="#">Menu item 4</a></li>
-    </ul>
-  </li>
-  <li class="menu menu-right menu-click">
-    <a class="button" href="http://localhost/php-project-2/Donatos/login.php">Logout</a>
-  </li>
-  <?php break;
-  case 'Dainius':?>
-   <li class="menu menu-hover">
-    <a href="#" class="button">Dainiaus</a>
-    <ul class="submenu">
-      <li><a href="#">Menu item 1</a></li>
-      <li><a href="#">Menu item 2</a></li>
-      <li><a href="#">Menu item 3</a></li>
-      <li><a href="#">Menu item 4</a></li>
-    </ul>
-  </li>
-  <li class="menu menu-right menu-click">
-    <a class="button" href="http://localhost/php-project-2/Donatos/login.php">Logout</a>
-  </li>
-  <?php break;
-  case 'Sarunas':?>
-  <li class="menu menu-hover">
-    <a href="#" class="button">Šaruno</a>
-    <ul class="submenu">
-      <li><a href="#">Menu item 1</a></li>
-      <li><a href="#">Menu item 2</a></li>
-      <li><a href="#">Menu item 3</a></li>
-      <li><a href="#">Menu item 4</a></li>
-    </ul>
-  </li>
-  <li class="menu menu-right menu-click">
-    <a class="button" href="http://localhost/php-project-2/Donatos/login.php">Logout</a>
-  </li>
-  <?php break;
-  case 'Nikolajus':?>
-  <li class="menu menu-hover">
-    <a href="#" class="button">Nikolajaus</a>
-    <ul class="submenu">
-      <li><a href="#">Menu item 1</a></li>
-      <li><a href="#">Menu item 2</a></li>
-      <li><a href="#">Menu item 3</a></li>
-      <li><a href="#">Menu item 4</a></li>
-    </ul>
-  </li>
-  <li class="menu menu-right menu-click">
-    <a class="button" href="http://localhost/php-project-2/Donatos/login.php">Logout</a>
-  </li>
-  <?php break;
-  default:?>
     <li class="menu menu-hover">
     <a href="#" class="button">Jono</a>
     <ul class="submenu">
@@ -152,6 +71,5 @@ case 'Marius':?>
   <li class="menu menu-right menu-click">
     <a class="button" href="http://localhost/php-project-2/Donatos/login.php">Login</a>
   </li>
-<?php } ?>
 </ul>
   </nav>
