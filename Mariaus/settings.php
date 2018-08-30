efc16c120cb68588515400c688362bf953b89fe8
DESKTOP-FUO0LAK\Marius <kriunas.marius@gmail.com>
2018-08-30 13:36:43 +0300

    atnaujinu

---
+++ Mariaus/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+$Settings = 
+[
+'url' => 'http://www.mariaus.test',
+// 'dbName' => 'marius',
+// 'dbUser' => 'root',
+// 'dbPass' => '123',
+// 'css' => ['stilius' => 'styles.css', 'styles-2.css', 'styles-3.css', 'styles-4.css']
+// 'js' => ['skriptas' => ]
+
+];
