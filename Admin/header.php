e0fd26601b99ce72907ea91df5f9c12235598cb1
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-08-31 14:32:41 +0300

    redagavimo galimybe

--- Admin/header.php
+++ Admin/header.php
@@ -18,20 +18,53 @@ $username = "root";
 $password = "123";
 
 $conn = mysqli_connect($servername, $username, $password, 'wordpress2');
+$_SESSION['antraste'] = '';
+$_SESSION['turinys'] = '';
+$_SESSION['pageID'] = '';
 
 $sql = "SELECT id FROM users WHERE Username='".$_SESSION['username']."'";
 $userID = mysqli_fetch_assoc(mysqli_query($conn, $sql));
 unset($sql);
 
+if (isset($_GET['pageID'])) {
+  $sql = "SELECT Antraste, Turinys FROM pages WHERE id='".$_GET['pageID']."'";
+  $result = mysqli_query($conn, $sql);
+  unset($sql);
+  $_SESSION['pageID'] = $_GET['pageID'];
+
+  if(mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        $_SESSION['antraste'] .= $row['Antraste'];
+        $_SESSION['turinys'] .= $row['Turinys'];
+        
+    }
+  } else {
+    echo "No title";
+  }
+}
+
 if(isset($_POST['upload-page'])) {
   $sql = "INSERT INTO pages (Antraste, Turinys, UserID)
   VALUES ('".$_POST['heading']."', '".$_POST['content']."', '".$userID['id']."')";
-  
+
   if (mysqli_query($conn, $sql)) {
      echo "New record created successfully";
   } else {
      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
+  
+}
+
+if(isset($_POST['update-page'])) {
+  $sql = "UPDATE pages SET Turinys='".$_POST['content']."', Antraste='".$_POST['heading']."' WHERE id=".$_SESSION['pageID'];
+
+  if (mysqli_query($conn, $sql)) {
+      echo "Record updated successfully";
+  } else {
+      echo "Error updating record: " . mysqli_error($conn);
+  }
+  
+  
   
 }
 
@@ -54,13 +87,13 @@ if(isset($_POST['upload-page'])) {
     <ul class="submenu">
 <?php 
 //!!! use of undefined constant !!!
-@$sql = "SELECT Antraste FROM pages WHERE UserID='".$userID[id]."'";
+@$sql = "SELECT Antraste, id FROM pages WHERE UserID='".$userID[id]."'";
 $result = mysqli_query($conn, $sql);
 unset($sql);
 
 if(mysqli_num_rows($result) > 0) {
    while($row = mysqli_fetch_assoc($result)) {
-       echo "<li class=\"usersPages\"><a href=".'#'.">".$row['Antraste']."</a></li>";
+       echo "<li class=\"usersPages\"><a href=\"http://localhost/php-project-2/Admin/index.php?pageID=".$row['id']."\">".$row['Antraste']."</a></li>";
    }
 } else {
    echo "<li><p>0 results</p></li>";
