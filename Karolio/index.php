db4279a83c6efdc9d33101dd453a22f806abe9a6
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-09-01 12:32:38 +0300

    gitignore update, istryniau papke Karolio, kuri buvo nenaudojama

--- Karolio/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../PhpConsole/__autoload.php';
-PhpConsole\Helper::register(); // it will register global PC class
-// ...
-$var = 'Debug';
-// PC::debug($var, 'tag');
-// PC::tag($var);
-
-
