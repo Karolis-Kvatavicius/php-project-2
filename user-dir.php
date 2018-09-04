2520ef46b8aeae44b180d06f0d2171f9a764d9b9
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-09-04 12:25:19 +0300

    user_directories

---
+++ user-dir.php
@@ -0,0 +1,19 @@
+<?php
+
+$dir = '..';
+$user_directories = [];
+$user_name = [];
+$user_path = [];
+
+
+$di = new RecursiveDirectoryIterator($dir);
+foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
+    if(basename($file) == 'user.txt') {
+    $user = ucfirst(strtolower(file_get_contents($file)));
+    $path = preg_replace('/user.txt$/', '', $filename);
+    preg_match('/\w+\\\\$/', $path, $path2);
+    $user_directories[$user] = $path2[0]; 
+    }
+}
+print_r($user_directories);
+// $user_directories[$_GET['User']];
