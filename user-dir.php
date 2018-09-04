7a85c818135bccfd96cadb726bbab08b4d6ad63e
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-09-04 12:39:31 +0300

    update

--- user-dir.php
+++ user-dir.php
@@ -10,8 +10,8 @@ $di = new RecursiveDirectoryIterator($dir);
 foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
     if(basename($file) == 'user.txt') {
     $user = ucfirst(strtolower(file_get_contents($file)));
-    $path = preg_replace('/user.txt$/', '', $filename);
-    preg_match('/\w+\\\\$/', $path, $path2);
+    $path = preg_replace('/\\\\user.txt$/', '/', $filename);
+    preg_match('/\w+\/$/', $path, $path2);
     $user_directories[$user] = $path2[0]; 
     }
 }
