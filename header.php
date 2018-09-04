c3169afbe252ebd33f5ee9a35544b81f1829106b
Donata-K <39554183+Donata-K@users.noreply.github.com>
2018-09-04 12:52:56 +0300

    .

--- header.php
+++ header.php
@@ -16,8 +16,8 @@ if (!$conn) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <link rel="stylesheet" href="<?=$user_directories[$_GET['User']]?>style.css">    
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?=$user_directories[$_GET['User']]?>styles.css">    
     <title>Home</title>
 </head>
 <body>
