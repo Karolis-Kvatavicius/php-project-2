de07caead47bafcb787aa967026a96e9bfa530b4
Donata-K <39554183+Donata-K@users.noreply.github.com>
2018-09-04 12:47:01 +0300

    .

--- header.php
+++ header.php
@@ -16,10 +16,8 @@ if (!$conn) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="<?=$user_directories[$_GET['User']]?>style.css"> // papildoma reiks dadet kreipiantis i settingus, is Karolio, kurio userio direktorija,
-   
-   
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="<?=$user_directories[$_GET['User']]?>style.css">    
     <title>Home</title>
 </head>
 <body>
