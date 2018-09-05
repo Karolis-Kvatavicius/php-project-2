d1154ebaf26a9f250c303212633e6df0275a79b0
nikoloyed <nikoloyed@gmail.com>
2018-09-05 12:04:15 +0300

    images and pages

--- migration/Nikolajus-images.sql
+++ migration/Nikolajus-images.sql
@@ -1,3 +1,2 @@
 INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
-        VALUES ('images/desktop.jpg', '1', '1', '7');INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
-        VALUES ('images/demotyvacija.lt_Dvi-fotkes-Nx-dvi-juk-mum-uztenka-vieno-tokio-baisaus-snukio_137163402361.jpg', '1', '1', '8');
+        VALUES ('images/desktop.jpg', '1', '1', '7');
