eb98a3de3734b5acac17c522f7b848b53d0f2c5c
nikoloyed <nikoloyed@gmail.com>
2018-09-05 12:32:24 +0300

    asgnfpas

--- migration/Nikolajus-images.sql
+++ migration/Nikolajus-images.sql
@@ -1,2 +1,8 @@
 INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
-        VALUES ('images/desktop.jpg', '1', '1', '7');
+        VALUES ('images/desktop.jpg', '1', '1', '7');INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
+        VALUES ('images/krokodilas.jpg', '1', '1', '1');INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
+        VALUES ('images/800px-Moose_superior.jpg', '1', '1', '8');INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
+        VALUES ('images/desktop.jpg', '1', '1', '2');INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
+        VALUES ('images/png-images-mario-transparent-background-1473.png', '1', '1', '4');INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
+        VALUES ('images/talmaciba.jpg', '2', '2', '4');INSERT INTO Images (Nuoroda, PageId, ImageID, UserId ) 
+        VALUES ('images/Sarunas_Jasikevicius_by_Augustas_Didzgalvis.jpg', '1', '1', '3');
